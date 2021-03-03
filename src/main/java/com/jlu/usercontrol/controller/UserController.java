866537723c71a83aeb3ab8a97f64fe490943b016
Joshua Luppes <cooljoebob64@gmail.com>
2021-03-03 14:51:19 -0600

    Add API description to Swagger documentation

--- src/main/java/com/jlu/usercontrol/controller/UserController.java
+++ src/main/java/com/jlu/usercontrol/controller/UserController.java
@@ -4,6 +4,7 @@ import com.jlu.usercontrol.config.SwaggerConfig;
 import com.jlu.usercontrol.model.User;
 import com.jlu.usercontrol.repository.UserRepository;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +23,7 @@ public class UserController {
     UserRepository userRepository;
 
     @GetMapping("/users")
+    @ApiOperation(value="Get the list of all users, optionally filtered by state", response=User.class, responseContainer = "List")
     public List<User> getUsers(@RequestParam(value = "state", required = false) String state) {
         if (state != null) {
             return (List<User>) userRepository.findByState(state);
@@ -30,11 +32,13 @@ public class UserController {
     }
 
     @GetMapping("/users/{id}")
+    @ApiOperation(value="Get one particular user by specifying an Id", response=User.class)
     public Optional<User> getUserById(@PathVariable(value = "id") Long id) {
         return userRepository.findById(id);
     }
 
     @PostMapping("/users")
+    @ApiOperation(value="Create a new user, pending validation")
     public ResponseEntity<Void> createUser(@RequestBody @Valid User user, BindingResult bindingResult) {
         if(userRepository.findById(user.getId()).isPresent()){
             bindingResult.rejectValue("userId", "error.userId", "User Id is already taken");
@@ -48,11 +52,13 @@ public class UserController {
     }
 
     @PutMapping("/users/{id}")
+    @ApiOperation(value="Update a user with a supplied object")
     public void updateUser(@PathVariable(value = "id") Long id, @RequestBody User user) {
         userRepository.save(user);
     }
 
     @DeleteMapping("/users/{id}")
+    @ApiOperation(value="Delete a user based on Id number")
     public void deleteUser(@PathVariable(value = "id") Long id) {
         userRepository.deleteById(id);
     }
