8cd78a6fc5cc2f5458e00b25c5da7280d6b9115a
Joshua Luppes <cooljoebob64@gmail.com>
2021-03-03 16:16:58 -0600

    Update controller to provide correct HTTP status codes and messages

--- src/main/java/com/jlu/usercontrol/controller/UserController.java
+++ src/main/java/com/jlu/usercontrol/controller/UserController.java
@@ -25,32 +25,41 @@ public class UserController {
     UserRepository userRepository;
 
     @GetMapping("/users")
-    @ApiOperation(value="Get the list of all users, optionally filtered by state", response=User.class, responseContainer = "List")
-    public List<User> getUsers(@RequestParam(value = "state", required = false) String state) {
+    @ApiOperation(value = "Get the list of all users, optionally filtered by state", response = User.class, responseContainer = "List")
+    @ApiResponses(@ApiResponse(code = 200, message = "OK - Found list of users"))
+    public ResponseEntity<List<User>> getUsers(@RequestParam(value = "state", required = false) String state) {
         if (state != null) {
-            return (List<User>) userRepository.findByState(state);
+            return new ResponseEntity<>(userRepository.findByState(state), HttpStatus.OK);
         }
-        return (List<User>) userRepository.findAll();
+        return new ResponseEntity<>((List<User>) userRepository.findAll(), HttpStatus.OK);
     }
 
     @GetMapping("/users/{id}")
-    @ApiOperation(value="Get one particular user by specifying an Id", response=User.class)
-    public Optional<User> getUserById(@PathVariable(value = "id") Long id) {
-        return userRepository.findById(id);
+    @ApiOperation(value = "Get one particular user by specifying an Id", response = User.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK - Found user with specified Id"),
+            @ApiResponse(code = 404, message = "Not Found - No User found with the specified Id")
+    })
+    public ResponseEntity<Optional<User>> getUserById(@PathVariable(value = "id") Long id) {
+        Optional<User> foundUser = userRepository.findById(id);
+        if (foundUser.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(foundUser, HttpStatus.OK);
     }
 
     @PostMapping("/users")
-    @ApiOperation(value="Create a new user, pending validation")
-    @ApiResponses(value={
-            @ApiResponse(code=201, message="Created - Successfully created the new user"),
-            @ApiResponse(code=400, message="Bad Request - Supplied User object did not pass validation")
+    @ApiOperation(value = "Create a new user, pending validation")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Created - Successfully created the new user"),
+            @ApiResponse(code = 400, message = "Bad Request - Supplied User object did not pass validation")
     })
     public ResponseEntity<Void> createUser(@RequestBody @Valid User user, BindingResult bindingResult) {
-        if(userRepository.findById(user.getId()).isPresent()){
+        if (userRepository.findById(user.getId()).isPresent()) {
             bindingResult.rejectValue("userId", "error.userId", "User Id is already taken");
         }
 
-        if(bindingResult.hasErrors()){
+        if (bindingResult.hasErrors()) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
         userRepository.save(user);
@@ -58,14 +67,39 @@ public class UserController {
     }
 
     @PutMapping("/users/{id}")
-    @ApiOperation(value="Update a user with a supplied object")
-    public void updateUser(@PathVariable(value = "id") Long id, @RequestBody User user) {
+    @ApiOperation(value = "Update a user with a supplied object")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK - Successfully updated the user"),
+            @ApiResponse(code = 400, message = "Bad Request - Supplied User object did not pass validation"),
+            @ApiResponse(code = 404, message = "Not Found - Could not find a user with the specified Id")
+    })
+    public ResponseEntity<Void> updateUser(@PathVariable(value = "id") Long id, @RequestBody User user, BindingResult bindingResult) {
+        if (!userRepository.existsById(id)) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else if (bindingResult.hasErrors()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
         userRepository.save(user);
+        return new ResponseEntity<>(HttpStatus.OK);
+
     }
 
     @DeleteMapping("/users/{id}")
-    @ApiOperation(value="Delete a user based on Id number")
-    public void deleteUser(@PathVariable(value = "id") Long id) {
-        userRepository.deleteById(id);
+    @ApiOperation(value = "Delete a user based on Id number")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK - Successfully deleted the user"),
+            @ApiResponse(code = 404, message = "Not Found - Could not find a user with the specified Id")
+    })
+    public ResponseEntity<Void> deleteUser(@PathVariable(value = "id") Long id) {
+        Optional<User> findUser = userRepository.findById(id);
+
+        if (findUser.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        userRepository.deleteById(findUser.get().getId());
+        return new ResponseEntity<>(HttpStatus.OK);
+
     }
 }
