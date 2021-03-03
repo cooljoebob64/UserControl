e09cad1eea67e3df1bd3bda7240b5920b24f0581
Joshua Luppes <cooljoebob64@gmail.com>
2021-03-03 09:32:40 -0600

    Add response value to user creation endpoint

--- src/main/java/com/jlu/usercontrol/controller/UserController.java
+++ src/main/java/com/jlu/usercontrol/controller/UserController.java
@@ -3,8 +3,12 @@ package com.jlu.usercontrol.controller;
 import com.jlu.usercontrol.model.User;
 import com.jlu.usercontrol.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,8 +32,16 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user) {
+    public ResponseEntity<Void> createUser(@RequestBody @Valid User user, BindingResult bindingResult) {
+        if(userRepository.findById(user.getId()).isPresent()){
+            bindingResult.rejectValue("userId", "error.userId", "User Id is already taken");
+        }
+
+        if(bindingResult.hasErrors()){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
         userRepository.save(user);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @PutMapping("/users/{id}")
--- src/main/java/com/jlu/usercontrol/model/User.java
+++ src/main/java/com/jlu/usercontrol/model/User.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Size;
 
 @Data
 @Builder
@@ -21,7 +22,12 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Size(max=20)
     private String firstName;
+
+    @Size(min=2)
     private String lastName;
+
+    @Size(min=4, max=20)
     private String state;
 }
