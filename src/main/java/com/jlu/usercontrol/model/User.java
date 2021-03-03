92beaf0db004c03c26c0f101e9fed036a0d32f99
Joshua Luppes <cooljoebob64@gmail.com>
2021-03-03 15:56:29 -0600

    Add field descriptions to User model

--- src/main/java/com/jlu/usercontrol/model/User.java
+++ src/main/java/com/jlu/usercontrol/model/User.java
@@ -1,5 +1,6 @@
 package com.jlu.usercontrol.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,14 +21,18 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @ApiModelProperty(notes="The User's Id number")
     private Long id;
 
     @Size(max=20)
+    @ApiModelProperty(notes="The User's first name")
     private String firstName;
 
     @Size(min=2)
+    @ApiModelProperty(notes="The User's last name")
     private String lastName;
 
     @Size(min=4, max=20)
+    @ApiModelProperty(notes="The User's state of residence")
     private String state;
 }
